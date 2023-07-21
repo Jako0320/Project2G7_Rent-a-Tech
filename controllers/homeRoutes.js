697d65c388e1e9e84dcd3b8d70f54fa0393b891e
Jacques <jacques.scoffoni@gmail.com>
2023-07-20 21:24:04 -0400

    Merge branch 'main' of https://github.com/Jako0320/Project2G7_Rent-a-Tech

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,104 @@
-const router = require("express").Router()
+const router = require("express").Router();
+const { User, Tech, Workorder } = require('../models');
 
-router.get("/",(req,res)=>{
-  res.render("landing")
-})
 
-router.get("/userlogin",(req,res)=>{
-  res.render("userlogin")
-})
+// router.get("/",(req,res)=>{
+//   res.render("landing")
+// })
 
-router.get("/techlogin",(req,res)=>{
-  res.render("techlogin")
-})
+// router.get("/userlogin",(req,res)=>{
+//   res.render("userlogin")
+// })
+
+// router.get("/techlogin",(req,res)=>{
+//   res.render("techlogin")
+// })
+
+router.get('/', async (req, res) => {
+try {
+  const workOrderData = await Workorder.findAll({
+    include: [
+      {
+        model: User,
+        attributes: ['name'],
+      },
+      {
+        model: Tech,
+        attributes: ['name'],
+      }
+    ],
+  });
+  const workOrder = workOrderData.map((workorder) => workorder.get({ plain: true }));
+
+    res.render('landing', { 
+      workOrder, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/workorder/:id', async (req, res) => {
+  try {
+    const workOrderData = await Workorder.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+        {
+          model: Tech,
+          attributes: ['name'],
+        }
+      ],
+    });
+
+    const workorder = workOrderData.get({ plain: true });
+
+    res.render('workorder', {
+      ...workorder,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/userlogin', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/userprofile');
+    return;
+  }
+
+  res.render('userlogin');
+});
+
+router.get('/techlogin', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/techprofile');
+    return;
+  }
+
+  res.render('techlogin');
+});
+
+module.exports = router;
