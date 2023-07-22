e6e0d0dd1e0978fda57f66a3706297f733d1c79e
Chiko repo <124736255+chikogit@users.noreply.github.com>
2023-07-22 09:09:05 -0400

    new data base changes 07/21

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
-DROP DATABASE IF EXISTS techs_db;
-CREATE DATABASE techs_db;
+CREATE DATABASE RENT_A_TECH
 CREATE TABLE users (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL
 );
+Insert into users values(3,'Allen Ramcharam', 'muzamil@gmail.com', '1234rob')
 CREATE TABLE technicians (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
@@ -15,9 +15,8 @@ CREATE TABLE technicians (
 CREATE TABLE workorders (
   id INT PRIMARY KEY AUTO_INCREMENT,
   user_id INT NOT NULL,
-  worker_id INT NOT NULL,
-  service_id INT NOT NULL,
-  booking_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  technicians_id INT NOT NULL,
+ booking_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (technicians_id) REFERENCES technicians(id)
   );
